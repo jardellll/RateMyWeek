fe6b5ea8f2e639e1ca314d1527c7682cc07d1d24
jardellll <76527612+jardellll@users.noreply.github.com>
2024-06-07 06:28:28 -0400

    Initial Commit

---
+++ RateMyWeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RateMyWeek
+//
+//  Created by Jardel Emile on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RateMyWeek/RateMyWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyWeekApp.swift
+//  RateMyWeek
+//
+//  Created by Jardel Emile on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RateMyWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
